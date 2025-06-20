ba60a213b9d351e8b31a9cd9765b9514a3e52c2c
JluDev11 <jludev12@gmail.com>
2025-06-20 21:47:09 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/jluore/', // 👈 IMPORTANT!
   plugins: [react()],
 })
